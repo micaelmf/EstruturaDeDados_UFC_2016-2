d369568510c1e731f70440a8e48cc70b403f4fcc
Micael Ferreira <micaelmf@gmail.com>
2016-09-02 10:01:42 -0300

    Trabalho 2 - Criando classes e definindo atributos

---
+++ Trabalhos_ED/src/_02_1/MeuArrayList.java
@@ -0,0 +1,31 @@
+package _02_1;
+
+public class MeuArrayList {
+	private Object item;
+	private int tamanho;
+	private int ultimo;
+	
+	
+	
+	
+	public Object getItem() {
+		return item;
+	}
+	public void setItem(Object item) {
+		this.item = item;
+	}
+	public int getTamanho() {
+		return tamanho;
+	}
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+	public int getUltimo() {
+		return ultimo;
+	}
+	public void setUltimo(int ultimo) {
+		this.ultimo = ultimo;
+	}
+	
+	
+}
