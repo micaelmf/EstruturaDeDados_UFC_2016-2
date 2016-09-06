a3a0b56684e0c92cebbeffa800d36e2ed2b5daad
Micael Ferreira <micaelmf@gmail.com>
2016-09-06 11:23:34 -0300

    primeiros testes na classe principal

--- Trabalhos_ED/src/_02_1/Principal.java
+++ Trabalhos_ED/src/_02_1/Principal.java
@@ -2,8 +2,20 @@ package _02_1;
 
 public class Principal {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception {
+		MeuArrayList lista = new MeuArrayList(2);
+		
+		Integer a = new Integer(1);
+		Integer b = new Integer(2);
+		Integer c = new Integer(3);
+		
+		lista.insere(a);
+		lista.insere(b);
+		lista.insere(c);
+		
+		
+		
+		System.out.println(lista.getItem());
 	}
 
 }
