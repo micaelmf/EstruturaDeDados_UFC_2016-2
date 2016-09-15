0effefabe1b47458b1e27ddf3e544a95f58375a0
Micael Ferreira <micaelmf@gmail.com>
2016-09-15 00:23:15 -0300

    Trabalho 03 - muitas alterações

--- Trabalhos_ED/src/_3_1_ListaEncadeada/MinhaListaEncadeada.java
+++ Trabalhos_ED/src/_3_1_ListaEncadeada/MinhaListaEncadeada.java
@@ -1,16 +1,16 @@
 package _3_1_ListaEncadeada;
 
-public class MinhaListaEncadeada {
+public class MinhaListaEncadeada <T> {
 	private Celula primeiro;
 	private Celula ultimo;
-	
-	public void criaLista(){
+
+	public MinhaListaEncadeada(){
 		primeiro = new Celula();
 		ultimo = primeiro;
 		
 	}
 	
-	public boolean insere(Object item){
+	public boolean insere(T item){
 		ultimo.setProx(new Celula());
 		ultimo = ultimo.getProx();
 		ultimo.setValor(item);
@@ -25,9 +25,12 @@ public class MinhaListaEncadeada {
 		
 		while(aux != null && i < indice){
 			if(i+1 == indice){
-				//remove
 				Object item = aux.getProx().getValor();
+				//remove
 				aux.setProx(aux.getProx().getProx());
+				if(i+1 == tamanho()){
+					this.ultimo = aux;
+				}
 				return item;
 			}else{
 				i++;
@@ -37,7 +40,7 @@ public class MinhaListaEncadeada {
 		return null;
 	}
 	
-	public int size(){
+	public int tamanho(){
 		Celula aux = primeiro;
 		int i;
 		for(i = -1; aux != null; i++){
@@ -46,11 +49,53 @@ public class MinhaListaEncadeada {
 		return i;
 	}
 	
-	public Object get(int index){
+	public Object pesquisa(int indice){
 		Celula aux = primeiro;
-		for(int i = -1; aux != null && i < index; i++){
+		for(int i = -1; aux != null && i < indice; i++){
 			aux = aux.getProx();
 		}
 		return aux.getValor();		
+	}
+	
+	public void imprime(){
+		Celula aux = primeiro;
+		
+		int i = 0, t = tamanho();
+		System.out.print("[");
+		while(i < t){
+			System.out.print(aux.getProx().getValor());
+			aux = aux.getProx();
+			if(aux.getProx() == null){
+				System.out.println("]\n");
+			}else{
+				System.out.print(" - ");
+			}
+			i++;
+		}
+		
+	}
+	
+	public Object ultimo(){
+		return getUltimo().getValor();
+	}
+	
+	public Object primeiro(){
+		return getPrimeiro().getProx().getValor();
+	}
+	
+	public Celula getPrimeiro() {
+		return primeiro;
+	}
+	
+	public void setPrimeiro(Celula primeiro) {
+		this.primeiro = primeiro;
+	}
+	
+	public Celula getUltimo() {
+		return ultimo;
+	}
+	
+	public void setUltimo(Celula ultimo) {
+		this.ultimo = ultimo;
 	}
 }
--- Trabalhos_ED/src/_3_1_ListaEncadeada/Principal.java
+++ Trabalhos_ED/src/_3_1_ListaEncadeada/Principal.java
@@ -4,15 +4,36 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MinhaListaEncadeada lista = new MinhaListaEncadeada();
-		lista.criaLista();
+		MinhaListaEncadeada<Integer> lista = new MinhaListaEncadeada<Integer>();
+		//lista.criaLista();
 		lista.insere(1);
-		lista.insere(10);
+		lista.insere(2);
 		lista.insere(3);
+		lista.insere(4);
+		lista.insere(5);
+		lista.insere(6);
+		/*
 
-		System.out.println(lista.get(1));
+		 */
+		System.out.println("Tamanho: " + lista.tamanho());
+		System.out.println("Primeiro: " + lista.primeiro());
+		System.out.println("Ultimo: " + lista.ultimo());
+		lista.imprime();
 		
-		System.out.println(lista.size());
+		System.out.println("Item | " + lista.remove(0) + " | Removido");
+		/*
+		System.out.println("Item | " + lista.remove(4) + " | Removido");
+		System.out.println("Item | " + lista.remove(3) + " | Removido");
+		System.out.println("Item | " + lista.remove(2) + " | Removido");
+		System.out.println("Item | " + lista.remove(1) + " | Removido");
+		System.out.println("Item | " + lista.remove(0) + " | Removido");
+		 */
+		System.out.println("Tamanho: " + lista.tamanho());
+		System.out.println("Primeiro: " + lista.primeiro());
+		System.out.println("Ultimo: " + lista.ultimo());
+		lista.imprime();
+		
+		System.out.println("Pesquisa: " + lista.pesquisa(1));		
 	}
 
 }
