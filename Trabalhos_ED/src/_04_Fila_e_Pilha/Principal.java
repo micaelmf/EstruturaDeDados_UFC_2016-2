e0d1e5623d464280912d44a0f7c179bf415963ef
Micael Ferreira <micaelmf@gmail.com>
2016-09-23 08:52:57 -0300

    Criando pacote do trabalho 4

---
+++ Trabalhos_ED/src/_04_Fila_e_Pilha/Principal.java
@@ -0,0 +1,10 @@
+/*
+ * TRABALHO 04 - Emplementar quatro operações básica de: FILA com Arranjo, FILA Encadeada,
+ * FILA Circular, PILHA com Arranjo e PILHA encadeada e 
+ */
+
+package _04_Fila_e_Pilha;
+
+public class Principal {
+
+}
