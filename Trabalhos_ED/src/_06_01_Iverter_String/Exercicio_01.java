71622ed23b16e28ebcd45586331891a6818bb96d
Micael Ferreira <micaelmf@gmail.com>
2016-10-05 17:26:55 -0300

    Trabalho 06_1 - Inverter string usando pilhas PRONTA

--- Trabalhos_ED/src/_06_01_Iverter_String/Exercicio_01.java
+++ Trabalhos_ED/src/_06_01_Iverter_String/Exercicio_01.java
@@ -5,29 +5,28 @@
  */
 
 package _06_01_Iverter_String;
+
 import _04_3_PilhaArranjo.*;
 
-
 public class Exercicio_01 {
-	String[] pilhaString;
+	String[] aux;
 	MinhaPilhaArranjo a = new MinhaPilhaArranjo();
 	
-	public void le(String string){
-		pilhaString = string.split(" ");
-		a = pilhaString.;
-		
-		for(int i = a.tamanho(); i > 0; i--){
-			System.out.println("" + a.pesquisa(i));
-		}
-		
-		/*
-		for(int i = pilhaString.length; i > 0; i--){
-			System.out.print(pilhaString[i-1] + " ");
-		}
-		*/
-		
-
-	}
+	public void inverte(String string){
+		//Divite a string 
+		this.aux = string.split(" ");
 	
-
+		//Empilhando a string passada por parametro
+		for(int i = 0; i < aux.length; i++){
+			a.inseri(aux[i]);
+		}	
+		a.imprime();
+		
+		//Desempilhando dentro de uma nova pilha resultando na ordem inversa
+		MinhaPilhaArranjo b = new MinhaPilhaArranjo();
+		for(int i = 0; i < aux.length; i++){
+			b.inseri(a.removeUltimo());
+		}
+		b.imprime();
+	}
 }
--- Trabalhos_ED/src/_06_01_Iverter_String/Principal.java
+++ Trabalhos_ED/src/_06_01_Iverter_String/Principal.java
@@ -9,9 +9,10 @@ package _06_01_Iverter_String;
 public class Principal {
 
 	public static void main(String[] args) {
-		Exercicio_01 a = new Exercicio_01();
+		Exercicio_01 pilha = new Exercicio_01();
 		
-		a.le("O fortaleza empatou");
+		pilha.inverte("um dois tres quatro cinco seis sete oito nove dez");
+		
 
 	}
 
