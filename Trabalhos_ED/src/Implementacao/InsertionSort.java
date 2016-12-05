83a8e2186a91dcc8093f25c5ce864b3787e438ec
Micaelmf <micaelmf@gmail.com>
2016-12-05 12:18:24 -0300

    alterando nome de variáveis

--- Trabalhos_ED/src/Implementacao/InsertionSort.java
+++ Trabalhos_ED/src/Implementacao/InsertionSort.java
@@ -2,23 +2,23 @@ package Implementacao;
 
 public class InsertionSort {
 	
-	public int[] insertion(int original[], int tamanho){
+	public int[] insertion(int vetor[], int tamanho){
 		int i, j, atual;
 		
 		for(i = 1; i < tamanho; i++){
-			atual = original[i];
+			atual = vetor[i];
 			j = i - 1;
 			
-			while((j >= 0) && (atual < original[j])){
-				original[j+1] = original[j];
+			while((j >= 0) && (atual < vetor[j])){
+				vetor[j+1] = vetor[j];
 				j = j-1;
 			}
 			
-			original[j+1] = atual;
+			vetor[j+1] = atual;
 		}
 		
-		imprimeVetor(original);
-		return original;
+		imprimeVetor(vetor);
+		return vetor;
 	}
 	
 	public void imprimeVetor(int vetor[]){
