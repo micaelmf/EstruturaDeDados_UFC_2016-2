ca19b0ef068a9004a61f030277c75f54a592c8ca
Micaelmf <micaelmf@gmail.com>
2016-12-05 19:32:32 -0300

    MergeSort - Pronto

--- Trabalhos_ED/src/Implementacao/MergeSort.java
+++ Trabalhos_ED/src/Implementacao/MergeSort.java
@@ -1,51 +1,80 @@
 package Implementacao;
 
 public class MergeSort {
-	  /**
-     * Algoritmo Merge Sort.
-     */
-    public static void mergeSort( Comparable [ ] a ) {
-        Comparable [ ] tmpArray = new Comparable[ a.length ];
-        mergeSort( a, tmpArray, 0, a.length - 1 );
-    }
-    
-    /**
-     * Método interno que faz chamadas recursivas
-     */
-    private static void mergeSort( Comparable [ ] a, Comparable [ ] tmpArray,
-            int left, int right ) {
-        if( left < right ) {
-            int center = ( left + right ) / 2;
-            mergeSort( a, tmpArray, left, center );
-            mergeSort( a, tmpArray, center + 1, right );
-            merge( a, tmpArray, left, center + 1, right );
-        }
-    }
-    
-    /**
-     * Metódo interno que ordena duas partes de um subarray.
-     */
-    private static void merge( Comparable [ ] a, Comparable [ ] tmpArray,
-            int leftPos, int rightPos, int rightEnd ) {
-        int leftEnd = rightPos - 1;
-        int tmpPos = leftPos;
-        int numElements = rightEnd - leftPos + 1;
-        
-        // loop principal
-        while( leftPos <= leftEnd && rightPos <= rightEnd )
-            if( a[ leftPos ].compareTo( a[ rightPos ] ) <= 0 )
-                tmpArray[ tmpPos++ ] = a[ leftPos++ ];
-            else
-                tmpArray[ tmpPos++ ] = a[ rightPos++ ];
-        
-        while( leftPos <= leftEnd )     
-            tmpArray[ tmpPos++ ] = a[ leftPos++ ];
-        
-        while( rightPos <= rightEnd )   
-            tmpArray[ tmpPos++ ] = a[ rightPos++ ];
-        
-        // Copiando o array temporário de volta
-        for( int i = 0; i < numElements; i++, rightEnd-- )
-            a[ rightEnd ] = tmpArray[ rightEnd ];
-    }
+	
+	public MergeSort() {
+		
+	}
+	
+	public void sort(int[] a) {
+		if (a.length < 2) {
+			return;
+		}
+		int tam = a.length;
+		int[] aux = new int[tam];
+		mergesort(a, aux, 0, tam-1);
+	}
+	
+	public void mergesort(int[]vetor, int[]aux, int lo, int hi) {
+		//
+		if(lo < hi) {
+			int mid = (lo + hi) / 2;
+			mergesort(vetor, aux, lo, mid);
+			mergesort(vetor, aux, (mid + 1), hi);
+			merge(vetor, aux, lo, mid, hi);
+		}
+	}
+	
+	public void merge(int[]vetor, int[]aux, int lo, int mid, int hi) {
+		int i = lo;
+		int j = mid + 1;
+		
+		//Stopwatch tempo = new Stopwatch();
+		
+		for (int k = lo; k <= hi; k++) {
+			aux[k] = vetor[k];
+		}
+		
+		for (int k = lo; k <= hi; k++) {
+			if (i > mid)								//se o primeiro metade acabar, o vetor meio copia os elementos da segunda metada para o vetor
+				vetor[k] = aux[j++];					
+			else if (j > hi)							//se a segunda metade acabar, copia os elementos da primeira metade
+				vetor[k] = aux[i++];		 
+			else if (aux[i] < aux[j])					//se ainda tem elementos nas metades, compara se sao menores e guarda
+				vetor[k] = aux[i++];
+			else 										// ou guarda o restante na outr parte
+				vetor[k] = aux[j++];
+			
+		}
+		//double tempoT = tempo.elapsedTime();
+		//System.out.println(tempoT + " nanosegundos");
+		
+	}
+	
+	
+	public void exibir(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+	
+	public void imprimeVetor(int vetor[]){
+		for(int i=0; i < vetor.length; i++){
+			System.out.printf(" " + vetor[i]);
+		}
+		System.out.println("");
+		
+	}	
+
+	/*
+	public static int[] gerarAleatorios(int[]a ) {
+		for(int i = 0; i < a.length; i++) {
+			a[i] = -20000 + (int) (Math.random()*(20000-(-20000)));
+			//System.out.println(aleatorio[i]);
+			
+		}
+		return a;
+	}
+	 */
 }
--- Trabalhos_ED/src/Teste/TesteMergeSort.java
+++ Trabalhos_ED/src/Teste/TesteMergeSort.java
@@ -1,9 +1,20 @@
 package Teste;
 
+import Implementacao.MergeSort;
+
 public class TesteMergeSort {
 
 	public static void main(String[] args) {
+		MergeSort merge = new MergeSort();
 		
+		
+		int[] vetor = {8,7,3,6,5,4,9,1,10,2};
+		
+		merge.imprimeVetor(vetor);
+		
+		merge.sort(vetor);
+
+		merge.imprimeVetor(vetor);
 
 	}
 
